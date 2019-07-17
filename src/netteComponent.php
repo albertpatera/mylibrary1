850062f4cc05cfafb4ca345b59a052ce49b18cb3
Albert Patera <pateraa@vlp.cz>
2019-07-17 11:37:32 +0200

    First commit to init base repo structure

---
+++ src/netteComponent.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Buonzz\Template;
+use Buonzz\Template\YourClass AS Base;
+use Nette;
+
+class netteComponent extends Nette\Application\UI\Presenter
+{
+    protected function createComponentBase()
+    {
+        $baseComp = new Nette\Application\UI\Form;
+
+
+        return $baseComp;
+
+    }
+}
